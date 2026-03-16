664e1452522f42911085660da06550646b9e8a55
nicolasgarea <nicogareacidre@gmail.com>
2026-03-16 12:31:06 +0100

    feat: add review routes
    closes #11

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.routes.auth_routes import router as auth_router
 from app.routes.user_routes import router as user_router
+from app.routes.review_routes import router as review_router
 
 
 def register_routers(app: FastAPI):
     app.include_router(auth_router)
     app.include_router(user_router)
+    app.include_router(review_router)
