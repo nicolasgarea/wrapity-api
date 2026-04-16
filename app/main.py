250bedcbbba815af957bf98a62f63cfeb80b7ed3
Nicolás Garea <nicogareacidre@gmail.com>
2026-04-16 14:45:23 +0200

    chore: configure cors for development (#41)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from app.core.exception_handlers import register_exception_handlers
 from app.routes import register_routers
-import app.models
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Wrapity", description="Music-based social network API", version="0.1.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 register_exception_handlers(app)
